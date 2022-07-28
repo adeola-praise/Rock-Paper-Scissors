66d289973b001e235f9449f9173895f34de34b3d
Adeola Praise <adeolapraise02@gmail.com>
2022-07-28 14:00:50 +0100

    Complete the Console version of the game

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Rock-Paper-Scissors
+
+This project has taught me how to use the switch statement and to create functions.
--- scripts.js
+++ scripts.js
@@ -1,10 +1,12 @@
 // A function that creates random choices from an array
-
 function getComputerChoice(){
     let choiceArray = ["ROCK", "PAPER", "SCISSORS"];
     let computerChoice = choiceArray[(Math.floor(Math.random() * choiceArray.length))]
     return(computerChoice);
 }
+
+let playerScore = 0;
+let computerScore = 0;
 
 // Function that plays one round of the game
 function playRound(computerSelection, playerSelection){
@@ -12,57 +14,71 @@ function playRound(computerSelection, playerSelection){
 
     switch (true){
         case (playerSelection == "ROCK" && computerSelection == "PAPER"):
+            computerScore++;
             result = "You Lose! Paper beats Rock";
             break;
 
         case (playerSelection == "PAPER" && computerSelection == "ROCK"):
+            playerScore++;
             result = "You Win! Paper beats Rock";
             break;
         
         case (playerSelection == "SCISSORS" && computerSelection == "ROCK"):
+            computerScore++;
             result = "You Lose! Rock beats Scissors";
             break;
         
         case (playerSelection == "ROCK" && computerSelection == "SCISSORS"):
+            playerScore++;
             result = "You Win! Rock beats Scissors";
             break;
             
         case (playerSelection == "PAPER" && computerSelection == "SCISSORS"):
+            computerScore++;
             result = "You Lose! Scissors beats Paper";
             break;
 
         case (playerSelection == "SCISSORS" && computerSelection == "PAPER"):
+            playerScore++;
             result = "You Win! Scissors beats Paper";
             break;
 
         case (playerSelection == computerSelection):
+            computerScore++;
+            playerScore++;
             result = "That was a draw";
             break;
         
         default:
             result = "Wrong input!";
+            alert("Wrong input!");
     }
 
-    document.getElementById('result').innerHTML = result;
-    console.log(result);
+    alert(result);
     return(result);
 }
 
-// Getting the computer's choice
-let computerSelection = getComputerChoice();
-console.log(computerSelection);
+function Game(){
 
-// Recieve the input when the user clicks the submit button
-document.getElementById('submit').onclick = function(){
+    for (let i = 0; i < 5; i++) {
 
-    // Getting the user input from the text box
-    let playerInput = document.getElementById('input').value;
+        // Getting the computer's choice
+        let computerSelection = getComputerChoice();
+        console.log(computerSelection);
 
-    // Convert the user's input to the case of the program
-    let playerSelection  = playerInput.toUpperCase();
+        // Getting the player's input from the text box
+        let playerInput = prompt("ROCK-PAPER-SCISSORS");
 
-    console.log(playerSelection);
+        // Convert the user's input to the case of the program
+        playerSelection = playerInput.toUpperCase();
 
-    (playRound(computerSelection, playerSelection));
+        (playRound(computerSelection, playerSelection));
+    }
 }
+
+// Call the game function
+Game();
+
+// Display the winner at the end of the game
+alert(`You: ${playerScore} | Computer: ${computerScore}`);
 
