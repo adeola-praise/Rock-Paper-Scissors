159db1bc55d7f2c6be749c5da704a325b6fcaac2
adeola-praise <68433446+adeola-praise@users.noreply.github.com>
2022-08-04 18:18:45 +0100

    Make RPS Images Responsive to Respective Buttons

--- scripts.js
+++ scripts.js
@@ -8,26 +8,70 @@ function getComputerChoice(){
 let playerScore = 0;
 let computerScore = 0;
 
-// Getting the computer's choice
+// Get the computer's choice
 let computerSelection = getComputerChoice();
 console.log(computerSelection);
 
 // Get rps buttons
 let rpsBtn = document.querySelectorAll('button');
 
-// Loop through the buttons
+// Loop through the rps buttons
 rpsBtn.forEach(function(btn){
     // Assign the value of any button clicked to playerSelection
-    item.onclick = function(){
+    btn.onclick = function(){
         let playerSelection = btn.value;
         // Call the playRound function
         playRound(computerSelection, playerSelection);
-    }  
+        ShowCompImg(computerSelection);
+        ShowPlayerImg(playerSelection);
+        console.log(imgDiv.lastChildElement)
+    }
+
 })
+
+// Create image element for player's choice
+let playerImg = document.createElement('img');
+
+// Create image element for computer's choice
+let compImg = document.createElement('img');
+
+// Select the image div
+let imgDiv = document.querySelector('.choiceimg')
+
+function ShowCompImg(computerSelection) {
+    if(computerSelection == 'ROCK'){
+        compImg.src = "./images/rock.png"
+        imgDiv.appendChild(compImg);
+     }
+     else if (computerSelection == 'PAPER') {
+        compImg.src = "./images/paper.png"
+        imgDiv.appendChild(compImg);
+     } 
+     else if(computerSelection == 'SCISSORS'){
+        compImg.src = "./images/scissors.png"
+        imgDiv.appendChild(compImg);
+     }
+}
+
+function ShowPlayerImg(playerSelection){
+
+     if(playerSelection == 'ROCK'){
+        playerImg.src = "./images/rock.png"
+        imgDiv.appendChild(playerImg);
+     }
+     else if (playerSelection == 'PAPER') {
+        playerImg.src = "./images/paper.png"
+        imgDiv.appendChild(playerImg);
+     } 
+     else if(playerSelection == 'SCISSORS'){
+        playerImg.src = "./images/scissors.png"
+        imgDiv.appendChild(playerImg);
+     }  
+}
 
 // Function that plays one round of the game
 function playRound(computerSelection, playerSelection){
-    
+
     let result = "";
 
     switch (true){
